4b8361ea0a1651efa38efe3bbd9b4f21a52efa77
Steven Newstead <steven.newstead@zonedigital.com>
2022-10-26 17:31:37 +0100

    Add hero with data

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,15 @@
+//pages/index.js
 import React from "react";
+import { Flex, View } from "@aws-amplify/ui-react";
 import { Hero } from "../ui-components";
-import { View } from "@aws-amplify/ui-react";
-
+import Layout from "../components/Layout";
 function index() {
   return (
-    <View>
-      <Hero />
-    </View>
+    <Layout handleClick={() => {}} authText="Sign Up" username="none">
+      <View marginBottom="135px">
+        <Hero handleClick={() => {}} />
+      </View>
+    </Layout>
   );
 }
 export default index;
