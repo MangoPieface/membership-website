2544f9818f33dec60b3ee687ae67bc76f646ea9e
Steven Newstead <steven.newstead@zonedigital.com>
2022-10-26 17:38:12 +0100

    Add all dynamic data

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,22 @@
 //pages/index.js
 import React from "react";
 import { Flex, View } from "@aws-amplify/ui-react";
-import { Hero } from "../ui-components";
+import { Footer, Hero, Persuade, TestimonyCollection } from "../ui-components";
 import Layout from "../components/Layout";
 function index() {
   return (
     <Layout handleClick={() => {}} authText="Sign Up" username="none">
       <View marginBottom="135px">
         <Hero handleClick={() => {}} />
+      </View>
+      <View>
+        <TestimonyCollection />
+      </View>
+      <Flex justifyContent={"center"}>
+        <Persuade banner="https://i.imgur.com/MxbD3N4.png" />
+      </Flex>
+      <View marginTop="50px" marginBottom="50px">
+        <Footer />
       </View>
     </Layout>
   );
